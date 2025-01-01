0e831ed06e88074fbe5b07875bac2605dd2987e6
Kazbonfim <zcry.games@gmail.com>
2025-01-01 05:00:01 -0300

    Havia uma rota que criei em um devaneio, além de não estar sendo usada, estava gastando recursos do servidor com sua inutilidade; deletado.

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const Task = require('./models/Tasks'); // Importando modelos
 const devRouter = require('./routes/dev');  // Roteador para dev
 const usersRouter = require('./routes/users'); // Roteador para usuários
 const dashboardRouter = require('./routes/dashboard'); // Roteador para dashboard
-const updateRouter = require('./routes/update');  // Roteador para update
 const adminRouter = require('./routes/admin'); // Roteador para administradores
 
 
@@ -46,13 +45,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', devRouter);  // Rota para a raiz do projeto
 app.use('/v1/users', usersRouter);  // Rota para usuários
 app.use('/v1/dashboard', dashboardRouter);  // Rota para o dashboard
-app.use('/v1/update', updateRouter);  // Rota para atualizar
 app.use('/v2/', adminRouter);  // Rota para administradores
 
 
 sequelize // Comente aqui
-  // sync()
-  .sync({ force: true }) // Apaga tudo
+  .sync()
+  //.sync({ force: true }) // Apaga tudo
   .then(async () => {
     console.log('Banco sincronizado com sucesso!');
   })
--- routes/users.js
+++ routes/users.js
@@ -1,105 +1,19 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/Users');
-const Task = require('../models/Tasks');
-const bcrypt = require('bcrypt');
+const userController = require('../controllers/userController');
+const { route } = require('./users');
 
-// Rota para exibir página de cadastro
-router.get('/register', function (req, res, next) {
-  // Sistema de notificações
-  const { showToast, message } = req.query;
-  const notification = showToast === 'true' ? { showToast, message } : null;
+router.get('/register', userController.register); //OK
 
-  res.render('register', { notification }); // Renderizar página de cadastros
-});
+router.post('/register', userController.registerPost); //OK
 
-// Rota para exibir informações do usuário
-router.get('/info/:id', async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const user = await User.findOne({ raw: true, where: { id: id } });
-    if (!user) {
-      return res.status(404).send('Usuário não encontrado');
-    }
-    res.render('user-info', { user });
-  } catch (error) {
-    console.error('Erro na rota /info/:id:' + error.message);
-    next(error);
-  }
-});
+router.get('/info/:id', userController.userInfo); //OK
 
-// Rota para editar o usuário
-router.get('/edit/:id', async (req, res, next) => {
-  try {
-    const { showToast, message } = req.query;
-    const notification = showToast === 'true' ? { showToast, message } : null;
-    const id = req.params.id;
-    const user = await User.findOne({ raw: true, where: { id: id } });
-    if (!user) {
-      return res.status(404).send('Usuário não encontrado');
-    }
-    res.render('user-update', { user, notification });
-  } catch (error) {
-    console.error('Erro na rota /update/:id' + error.message);
-    next(error);
-  }
-});
+router.get('/edit/:id', userController.userEdit); //OK
 
-// Rota para realizar o cadastro
-router.post('/register', async (req, res, next) => {
-  try {
-    console.log('🎲', req.body)
-    const { name, email, occupation, newsletter, hash } = req.body;
+router.post('/delete/:id', userController.userDelete); //OK
 
-    await User.create({
-      name,
-      email,
-      occupation,
-      newsletter: newsletter === 'on',
-      hash,
-    });
+router.post('/update/:id', userController.userUpdate); //OK
 
-    const notification = { showToast: true, message: 'Usuário cadastrado com sucesso!' };
-    res.status(303).redirect(`/v1/users/register?showToast=true&message=${encodeURIComponent(notification.message)}`);
-    
-  } catch (error) {
-    console.log('Erro ao salvar dados: ' + error.message);
-    console.log('Detalhes: ' + error);
-
-    const notification = { showToast: true, message: 'Erro ao realizar cadastro' };
-    res.status(303).redirect(`/v1/users/register?showToast=true&message=${encodeURIComponent(notification.message)}`);
-  }
-});
-
-// Rota para deletar o usuário
-router.post('/delete/:id', async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    await User.destroy({ where: { id } });
-    const notification = { showToast: true, message: 'Usuário deletado com sucesso!' };
-    res.status(303).redirect(`/v1/dashboard?showToast=true&message=${encodeURIComponent(notification.message)}`);
-  } catch (error) {
-    const notification = { showToast: true, message: 'Erro ao deletar o usuário!' };
-    res.status(303).redirect(`/v1/dashboard?showToast=true&message=${encodeURIComponent(notification.message)}`);
-  }
-});
-
-// Rota para atualizar o usuário
-router.post('/update/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { name, email, occupation, hash } = req.body;
-    const newsletter = req.body.newsletter === 'true';
-    const [updatedRows] = await User.update({ name, email, occupation, newsletter, hash }, { where: { id } });
-    if (updatedRows === 0) {
-      return res.status(404).send('Usuário não encontrado');
-    }
-    const notification = { showToast: true, message: 'Usuário atualizado com sucesso!' };
-    res.status(303).redirect(`/v1/dashboard?showToast=true&message=${encodeURIComponent(notification.message)}`);
-  } catch (error) {
-    const notification = { showToast: true, message: 'Erro ao atualizar o usuário!' };
-    res.status(303).redirect(`/v1/dashboard?showToast=true&message=${encodeURIComponent(notification.message)}`);
-  }
-});
 
 module.exports = router;
